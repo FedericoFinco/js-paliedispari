020cd1ada1ae8b56897806debd30edd644190d01
FedericoFinco <doppiaeffe94@gmail.com>
2023-04-21 18:12:34 +0200

    dev (main): all functionm done in console, comment for upgrading

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
 // let parolaUser=prompt("inserisci una parola")
 // let alorap=""
+const btnPalindromo=document.getElementById("btnPalindromo")
 
-// function palindromoCheck(parola){
-// for (let i = parola.length; i >= 1 ; i--) {
-//     alorap=(alorap+parola[i - 1])    
-// }
-// if (parolaUser==alorap){
-//     console.log(`la parola ${parolaUser} è un palindromo`)
-// }
-// else{
-//     console.log(`la parola ${parolaUser} NON è un palindromo`)
-// }
-// console.log(parola)
-// console.log(alorap)
-// }
-// palindromoCheck(parolaUser)
+btnPalindromo.addEventListener("click",function(){
+    let parolaUser=prompt("inserisci la parola")
+    while (parolaUser==("")){
+        parolaUser=prompt("inserisci almeno un carattere")
+    }
+    palindromoCheck(parolaUser)
+})
+
+function palindromoCheck(parola){
+    let alorap=""
+for (let i = parola.length; i >= 1 ; i--) {
+    alorap=(alorap+parola[i - 1])    
+}
+if (parola==alorap){
+    console.log(`la parola ${parola} è un palindromo`)
+}
+else{
+    console.log(`la parola ${parola} NON è un palindromo`)
+}
+console
+}
+
 
 
 
@@ -35,23 +44,23 @@ function isPariOrDispari(numero) {
 const btnPari=document.getElementById("btnPari")
 
 btnPari.addEventListener("click",function(){
-    let scelta=prompt("pari o dispari? scrivi la tua scelta")
+    let scelta=prompt("pari o dispari? scrivi la tua scelta")// to lower case
     while (scelta !==("pari") && scelta !==("dispari")) {
         scelta=prompt("pari o dispari? hai scritto altro")
     }
     let numeroUser=parseInt(prompt("inserisci un numero da 1 a 5"))
     
-    while (numeroUser < 1 || numeroUser > 5) {
+    while (numeroUser < 1 || numeroUser > 5) {   //while 1 || 5 || is nan e dentro if per log viersi
         numeroUser=parseInt(prompt("da 1 a 5 ho detto"))
     }
-    console.log(numeroUser)
+    console.log("il numero user è "+ numeroUser)
     while (isNaN(numeroUser)) {
         numeroUser=parseInt(prompt("un NUMERO da 1 a 5."))
     }
 
     let numeroPC=parseInt(getRandomNumber())
     let somma=numeroPC+numeroUser
-    console.log(somma)
+    console.log("la somma è "+ somma)
 
     if (scelta==("pari") && isPariOrDispari(somma)==false){
         alert("hai vinto")
@@ -65,14 +74,5 @@ btnPari.addEventListener("click",function(){
     
 })
 
-console.log(isPariOrDispari(3))
 
-// let numeroUser=prompt("inserisci numero")
-// numeroPC=getRandomNumber()
-
-// if (numeroUser<numeroPC){
-//     console.log("cacca")
-// }
-// else{
-//     console.log("pipi")
-// }
+//queryselect .checked per i radiobutton 
